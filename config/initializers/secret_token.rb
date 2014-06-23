edcfe98e564ba57aa5cb9524a95a1c3425c3dc30
Nicolas Marçal <nih.williams@gmail.com>
2014-06-22 21:34:43 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatToWear::Application.config.secret_token = '086e1ccae0cfb7b87afb541a0fe4d4a74a578dfda06de83d25ec2676e94e30c535308f76fa9a45a917ba879d9908a2155243434f82b269501241c7db966cf703'
